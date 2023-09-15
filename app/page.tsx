48111604d2a47883451ff55ce8ca501a261c7f83
YukinoU <yukino.12346789@gmail.com>
2023-09-15 12:32:50 +0900

    ガワ実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,159 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import cn from "classnames";
+import { Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [time, setTime] = useState("朝食");
+  const [number, setNumber] = useState(0);
+  const [taste, setTaste] = useState(Array<string>);
+  const [main, setMain] = useState("肉");
+  const [other, setOther] = useState("");
+  const router = useRouter();
+  const radioButton = ({
+    text,
+    value,
+    setValue,
+  }: {
+    text: string;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>;
+  }) => {
+    return (
+      <button
+        className={cn(
+          text == value
+            ? "bg-white text-main-red border-main-red"
+            : "bg-gray-200 text-gray-900 border-gray-500",
+          "flex items-center justify-center w-[84px] lg:w-[96px] py-2 text-xs lg:text-base font-bold rounded-full border"
+        )}
+        onClick={() => setValue(text)}
+      >
+        {text}
+      </button>
+    );
+  };
+  const tasteButton = ({ text }: { text: string }) => {
+    return (
+      <button
+        className={cn(
+          taste.length < number
+            ? "bg-white text-gray-900 border-gray-900"
+            : "bg-gray-200 text-gray-700 border-gray-500",
+          "flex items-center justify-center w-[84px] lg:w-[96px] py-2 text-xs lg:text-base font-bold rounded-full border"
+        )}
+        onClick={() => {
+          if (taste.length < number) setTaste([...taste, text]);
+        }}
+      >
+        {text}
+      </button>
+    );
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="w-full h-full flex flex-col justify-center">
+      <div className="bg-main-red h-20 text-sub-yellow flex items-center justify-center lg:text-2xl font-bold">
+        献立を立てる
+      </div>
+      <div>
+        <div className="pt-8 text-gray-900 lg:text-xl font-medium">
+          <div className="w-full pl-9">
+            <div className=" border-main-red border-b-2">時間帯</div>
+          </div>
+          <div className="flex gap-3 py-8 justify-center lg:gap-6">
+            {radioButton({ text: "朝食", value: time, setValue: setTime })}
+            {radioButton({ text: "昼食", value: time, setValue: setTime })}
+            {radioButton({ text: "夕食", value: time, setValue: setTime })}
+          </div>
+        </div>
+        <div className="pt-8 text-gray-900 lg:text-xl font-medium">
+          <div className="w-full pl-9">
+            <div className=" border-main-red border-b-2">品数</div>
+          </div>
+          <div className="flex gap-3 py-8 justify-center items-end font-medium">
+            <input
+              type="number"
+              value={number}
+              onChange={(e) => {
+                setNumber(parseInt(e.target.value));
+                if (taste.length > parseInt(e.target.value)) {
+                  setTaste(taste.slice(0, parseInt(e.target.value)));
+                }
+              }}
+              className="w-12 py-2 text-base rounded-lg border border-gray-500 text-center text-gray-900"
             />
-          </a>
+            品
+          </div>
+        </div>
+        <div className="pt-8 text-gray-900 lg:text-xl font-medium">
+          <div className="w-full pl-9">
+            <div className=" border-main-red border-b-2">味</div>
+          </div>
+          <div className="w-full pl-9 flex pt-2 gap-2">
+            {taste.map((t, index) => (
+              <div key={index}>
+                <button
+                  className="bg-white text-main-red border-main-red flex items-center justify-center w-[60px] lg:w-[72px] py-2 text-[10px] lg:text-xs font-bold rounded-full border-2"
+                  onClick={() => {
+                    setTaste(taste.filter((_, i) => i != index));
+                  }}
+                >
+                  {t}
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="lg:flex gap-3 justify-center lg:gap-6">
+            <div className="flex gap-3 lg:py-8 pt-8 pb-4 justify-center lg:gap-6">
+              {tasteButton({ text: "甘い" })}
+              {tasteButton({ text: "しょっぱい" })}
+              {tasteButton({ text: "辛い" })}
+            </div>
+            <div className="flex gap-3 pb-8 lg:py-8 justify-center lg:gap-6">
+              {tasteButton({ text: "苦い" })}
+              {tasteButton({ text: "酸っぱい" })}
+            </div>
+          </div>
+          <div className="pt-8 text-gray-900 lg:text-xl font-medium">
+            <div className="w-full pl-9">
+              <div className=" border-main-red border-b-2">メイン</div>
+            </div>
+            <div className="flex gap-3 py-8 justify-center lg:gap-6">
+              {radioButton({ text: "肉", value: main, setValue: setMain })}
+              {radioButton({ text: "魚", value: main, setValue: setMain })}
+              {radioButton({ text: "野菜", value: main, setValue: setMain })}
+            </div>
+          </div>
+          <div className="pt-8 text-gray-900 lg:text-xl font-medium">
+            <div className="w-full pl-9">
+              <div className=" border-main-red border-b-2">その他</div>
+              <div className="py-8 font-medium px-5 pr-9">
+                <textarea
+                  value={other}
+                  onChange={(e) => {
+                    setOther(e.target.value);
+                  }}
+                  className="w-full p-2 lg:text-base text-xs rounded-lg border border-gray-500 text-gray-900 h-20"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full px-[72px] pb-20">
+          <button
+            className="bg-main-red text-sub-yellow w-full py-4 text-lg font-bold rounded-2xl"
+            onClick={
+              () =>
+                router.push(
+                  "/recipe"
+                ) /* ここに献立を立てる処理を書く。レシピページに遷移する処理は一時的に入れている */
+            }
+          >
+            献立を立てる
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
